94b7c9db325b6264290cb4120b7975c549d9c2fe
Adam Di Cioccio <adamdicioccio@gmail.com>
2022-09-18 14:14:58 -0400

    Inital commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab1 {
+
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.WriteLine("This is the initial commit!");
+
+
+        }
+
+        public static void showMenu() {
+
+        }
+    }
+}
